0c5e51a2c1a486cb6dc9eedeb889d31f4ea59624
Leo Chen <Leo_Chen_zy@hotmail.com>
2024-09-10 17:36:21 +0800

    Stash updates for ng update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { SkeletonComponent } from './skeleton/skeleton.component';
-import { CompleteComponent } from './complete/complete.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: SkeletonComponent
-    },
-    {
-        path: 'complete', 
-        component: CompleteComponent
+        component: HomeComponent
     },
     {
         path: 'about',
